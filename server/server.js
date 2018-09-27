d76ee071540e9f2829862bd3500decbd6783d806
Mjones13 <jonesmal@umich.edu>
2018-09-27 11:25:35 -0700

    Added endpoint for posting a new artist w/ specific artistID

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const path = require('path');
-const { getArtist } = require('../database/index.js');
+const { getArtist, postArtist } = require('../database/index.js');
 const cors = require('cors');
 const server = express();
 
@@ -12,7 +12,16 @@ server.use(express.static(path.join(__dirname, '../public')));
 
 server.get('/artists/albums/:artistID', (req, res) => {
   getArtist(req.params.artistID, data => {
+    res.status(200);
     res.send(data);
+  });
+});
+
+server.post('/artists/albums/:artistID', (req, res) => {
+  const { artistID } = req.params;
+  postArtist(artistID, (err) => {
+    res.status(201);
+    res.send(`Created an artist at artistID with ${artistID}`);
   });
 });
 
