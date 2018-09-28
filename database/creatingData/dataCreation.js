57733f6e42135c7995a25e58d054872470373f15
Mjones13 <jonesmal@umich.edu>
2018-09-28 15:14:25 -0700

    Exported data generation to auxillary file, refactored for clarity

---
+++ database/creatingData/dataCreation.js
@@ -0,0 +1,51 @@
+const faker = require('faker');
+const coolImages = require('cool-images');
+
+const createArtists = (numToGenerate = 25000, currentArtistID) => {
+  const artists = [];
+  for (var i = 1; i < numToGenerate + 1; i++) {
+    let artist = {
+      artistID: currentArtistID + i,
+      artistName: faker.name.findName(),
+      albums: createAlbums(currentArtistID, i),
+    }
+    artists.push(artist);
+  }
+  return artists;
+};
+
+const createAlbums = (currentArtistID, index) => {
+  const albums = [];
+  const albumNumber = Math.floor(Math.random() * 10) + 1;
+  for (var j = 1; j < albumNumber + 1; j++) {
+    let album = {
+      albumID: (currentArtistID + index) * 100 + j,
+      albumName: faker.random.words(),
+      albumImage: coolImages.one(400, 400),
+      publishedYear: Math.floor(Math.random() * 69) + 1950,
+      songs: createSongs(currentArtistID, index, j),
+    }
+    albums.push(album);
+  }
+  return albums;
+};
+
+const createSongs = (currentArtistID, index, albumNumber) => {
+  const songs = [];
+  var songNumber = Math.floor(Math.random() * 21) + 9;
+  for (var k = 1; k < songNumber + 1; k++) {
+    let song = {
+      songID: (currentArtistID + index) * 1000 + albumNumber * 10 + k,
+      songName: faker.random.words(),
+      streams: Math.floor(Math.random() * 250000000),
+      length: Math.floor(Math.random() * 221) + 30,
+      popularity: Math.floor(Math.random() * 8) + 1,
+      addedToLibrary: faker.random.boolean()
+    }
+    songs.push(song);
+  }
+  return songs;
+}
+
+
+module.exports = { createArtists, createAlbums, createSongs };
