5bc54df0553cbeb7e7b73b43de8270acc3b0ec01
Mjones13 <jonesmal@umich.edu>
2018-09-29 16:18:47 -0700

    Added csv data conversion simultaneously w/ json (Broken)

--- database/creatingData/dataGeneration.js
+++ database/creatingData/dataGeneration.js
@@ -5,10 +5,12 @@ const ndjson = require('ndjson');
 const path = require('path');
 const { createArtists, createAlbums, createSongs } = require('./dataCreation.js');
 
-var generateData = (numToGenerate = 25000, currentArtistID) => {
+// var generateData = (numToGenerate = 25000, currentArtistID) => new Promise(async (resolve, reject) => {
+const generateData = (numToGenerate = 25000, currentArtistID) => {
   if (typeof numToGenerate !== 'number' || numToGenerate < 0) {
     return console.error('Requires a valid number of records to generate, please try again')
   }
+  // var allArtists = await createArtists(numToGenerate, currentArtistID, resolve);
   var allArtists = createArtists(numToGenerate, currentArtistID);
   currentArtistID += numToGenerate;
   return allArtists;
@@ -26,12 +28,12 @@ const loopRecords = (artists, outputStream) => new Promise((resolve, reject) => 
 
 const createRecords = async () => {
   console.log('--------Starting the data generation script---------')
-  for (var i = 0; i < 400; i++) {
-    let dataFile = path.join(__dirname + `/dataFiles/data${i}.json`);
+  for (var i = 0; i < 2; i++) {
+    let dataFile = path.join(__dirname + `/dataFiles/json/data${i}.json`);
     var outputStream = fs.createWriteStream(dataFile); // {flags: 'a'}
-    let artists = JSON.stringify(generateData(25000, currentArtistID));
+    let artists = JSON.stringify(generateData(10000, currentArtistID));
     await loopRecords(artists, outputStream);
-    currentArtistID += 25000;
+    currentArtistID += 10000;
     console.log(`Finished writing ${currentArtistID} files!`);
   }
   console.log('-------Finished running the data generation script--------');
