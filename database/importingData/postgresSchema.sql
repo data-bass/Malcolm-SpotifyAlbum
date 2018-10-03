c0d10a3c80f3e1ada85c32ec5cf5b8a19d7adc6c
Mjones13 <jonesmal@umich.edu>
2018-10-02 23:21:48 -0700

    Removed array of foreign keys & added drop table conditional

--- database/importingData/postgresSchema.sql
+++ database/importingData/postgresSchema.sql
@@ -1,33 +1,48 @@
-  
-  
-  CREATE TABLE Artists {
-    ArtistID int,
-    ArtistName varchar(80),
-    Albums integer[],
-    PRIMARY KEY (ArtistID),
-    FOREIGN KEY (Albums) REFERENCES Albums(Album)
-  }
+  DROP TABLE IF EXISTS songs;
+  DROP TABLE IF EXISTS albums;
+  DROP TABLE IF EXISTS artists;
 
-  CREATE TABLE Albums {
-    AlbumID int, 
-    AlbumName varchar(80),
-    AlbumImage varchar(150),
-    PublishedYear integer,
-    Songs integer [],
-    ArtistID integer, 
-    PRIMARY KEY (AlbumID),
-    FOREIGN KEY (Songs) REFERENCES Songs(SongID),
-    FOREIGN KEY (ArtistID) REFERENCES Artists(ArtistID)
-  }
+  CREATE TABLE artists (
+    artistID int,
+    artistName varchar(300),
+    -- albums integer[],
+    PRIMARY KEY (artistID)
+    -- FOREIGN KEY (albums) REFERENCES albums(Album)
+  );
 
-  CREATE TABLE Songs {
-    SongID int,
-    SongName varchar(80),
-    Streams integer,
-    Length integer,
-    Popularity integer,
+  -- COPY artists(artistID, artistName) FROM 'user/local/Desktop/Malcolm-SpotifyAlbum/database/creatingData/dataFiles/csv/artists/data0.csv' DELIMITER ',' CSV HEADER;
+
+  CREATE TABLE albums (
+    albumID int, 
+    albumName varchar(300),
+    albumImage varchar(150),
+    publishedYear integer,
+    -- songs integer [],
+    artistID integer, 
+    PRIMARY KEY (albumID)
+    -- FOREIGN KEY (songs) REFERENCES songs(SongID),
+    -- FOREIGN KEY (artistID) REFERENCES artists(artistID)
+  );
+
+  CREATE TABLE songs (
+    songID int,
+    songName varchar(300),
+    streams integer,
+    length integer,
+    popularity integer,
     addedToLibrary Boolean,
-    AlbumID number,
-    PRIMARY KEY (SongID),
-    FOREIGN KEY (AlbumID) REFERENCES Albums(AlbumID) 
-  }
+    albumID integer,
+    PRIMARY KEY (songID)
+    -- FOREIGN KEY (albumID) REFERENCES albums(albumID) 
+  );
+
+  -- ALTER TABLE artists ADD FOREIGN KEY(albums) REFERENCES albums(albumID);
+  ALTER TABLE albums ADD FOREIGN KEY(artistID) REFERENCES artists(artistID) ON DELETE CASCADE;
+  ALTER TABLE songs ADD FOREIGN KEY(albumID) REFERENCES albums(albumID) ON DELETE CASCADE;
+
+-- Join table query to select the information for a specific artist
+-- select * from artists
+-- join albums on artists.artistid=albums.artistid
+-- join songs on albums.albumid=songs.albumid
+-- where artists.artistid=999888;
+
