75888217f21d8a16c22dff3c1f0b6cdef73e6139
Mjones13 <jonesmal@umich.edu>
2018-10-07 16:41:34 -0700

    Fixed request endpoint to target local server

--- database/stressTesting/ksixScript.js
+++ database/stressTesting/ksixScript.js
@@ -2,17 +2,17 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-  vus: 10,
-  duration: "5s",
+  vus: 450,
+  duration: "10s",
 };
 
 export default () => {
   const randomArtistId = Math.floor(Math.random() * 8000000) + 1000000;
-  let res = http.get(`http://localhost:3001/artists/album/${randomArtistId}`);
+  let res = http.get(`http://localhost:3001/artists/albums/${randomArtistId}`);
   check(res, {
-    "status was 200": (r) => r.status === 200,
+    "status was 200": (r) => r.status == 200,
     "transaction time OK": (r) => r.timings.duration < 2000
   });
-  sleep(1);
+  // sleep(1);
 };
 
