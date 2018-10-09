7d5c4900a51552b4aa391c494cac986d02f6ff61
Mjones13 <jonesmal@umich.edu>
2018-10-08 22:32:56 -0700

    Added child processes for node server

--- server/index.js
+++ server/index.js
@@ -1,3 +1,18 @@
 const server = require('./server.js');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
 
-server.listen(3001, () => console.log("Listening on port 3001!"));
+if (cluster.isMaster) {
+  console.log(`Master ${process.pid} is running!`);
+
+  for (let i = 0; i < numCPUs; i++) {
+    cluster.fork();
+  }
+
+  cluster.on('exit', (worker, code, signal) => {
+    console.log(`worker ${worker.process.pid} died :(`);
+  });
+} else {
+  server.listen(3001, () => console.log("Listening on port 3001!"));
+  console.log(`Worker ${process.pid} started`);
+}
