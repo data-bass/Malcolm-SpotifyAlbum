8f01ff898d86e4745314f64e1913231216cb9def
Mjones13 <jonesmal@umich.edu>
2018-10-13 11:40:19 -0700

    Converted stress test to match Spotify use case

--- database/stressTesting/awsKsixScript..js
+++ database/stressTesting/awsKsixScript..js
@@ -2,13 +2,15 @@ import http from 'k6/http';
 import { check, sleep } from 'k6';
 
 export let options = {
-  vus: 50,
+  vus: 100,
   duration: "30s",
 };
 
 export default () => {
-  const randomArtistId = Math.floor(Math.random() * 6000000) + 1000000;
-  let res = http.get(`http://18.188.226.149:3001/artists/albums/${randomArtistId}`);
+  const getRandomArtistId = () => Math.floor(Math.random() * 6000000) + 1000000;
+  const randomArtistId = Math.random() * 10 < 7 ? 2222222 : getRandomArtistId();
+
+  let res = http.get(`http://18.222.27.94/artists/albums/${randomArtistId}`);
   check(res, {
     "status was 200": (r) => r.status == 200,
     "transaction time OK": (r) => r.timings.duration < 2000
