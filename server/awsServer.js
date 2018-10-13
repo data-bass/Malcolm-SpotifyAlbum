05403aa9c276ed648364d5001b302e44c50b7455
Mjones13 <jonesmal@umich.edu>
2018-10-13 11:41:36 -0700

    Removed uneccessary toobusy-js statements

--- server/awsServer.js
+++ server/awsServer.js
@@ -7,43 +7,41 @@ const request = require('superagent');
 
 const responseTime = require('response-time');
 const newRelic = require('newrelic');
-const cluster = require('cluster');
-const numCPUs = require('os').cpus().length;
+// const cluster = require('cluster');
+// const numCPUs = require('os').cpus().length;
 const { getArtist, postArtist, updateArtist, deleteArtist, redisClient, getCache, setCache } = require('../database/awsIndex.js');
 const server = express();
 
-if (cluster.isMaster) {
-  console.log(`Master ${process.pid} is running!`);
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
+// if (cluster.isMaster) {
+//   console.log(`Master ${process.pid} is running!`);
+//   for (let i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`worker ${worker.process.pid} died :(`);
-  });
-} else {
-  server.listen(3001, () => console.log("Listening on port 3001!"));
-  console.log(`Worker ${process.pid} started`);
-}
+//   cluster.on('exit', (worker, code, signal) => {
+//     console.log(`worker ${worker.process.pid} died :(`);
+//   });
+// } else {
+server.listen(3001, () => console.log("Listening on port 3001!"));
+console.log(`Worker ${process.pid} started`);
+// }
 
 
 
 
 //too busy js settings
-toobusy.maxLag(1500);
-toobusy.interval(1900);
+toobusy.maxLag(1000);
+toobusy.interval(1500);
 let currentMaxLag = toobusy.maxLag()
 let interval = toobusy.interval();
-toobusy.onLag((currentLag) => {
-  console.log('Event loop lag detected! Latency: ' + currentLag + 'ms');
-});
-server.use((rew, res, next) => {
-  if (toobusy()) {
-    res.send(503, 'Server is overloaded, please try again later. Sorry for any inconvenience!');
-  } else {
-    next();
-  }
-});
+
+// server.use((rew, res, next) => {
+//   if (toobusy()) {
+//     res.send(503, 'Server is overloaded, please try again later. Sorry for any inconvenience!');
+//   } else {
+//     next();
+//   }
+// });
 
 server.use(responseTime());
 
